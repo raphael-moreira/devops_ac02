153fdbbeea064c0959bf112b92c6e7aec3f83f9c
Raphael Moreira <61034183+raphael-moreira@users.noreply.github.com>
2020-04-23 13:53:19 -0300

    Update test_operacoes.py

--- com/raphael/test/test_operacoes.py
+++ com/raphael/test/test_operacoes.py
@@ -2,10 +2,11 @@ from unittest import TestCase
 from com.raphael.operacoes import Operacoes
 
 class TestOperacoes(TestCase):
+    
     def setUp(self):
         self.operacoes = Operacoes()
 
     def test_soma(self):
-        self.assertEqual(self.operacoes.soma([1,5]),6,"Should be 6")
+        self.assertEqual(self.operacoes.soma([1,3]),4,"Deveria ser 4")
 
         
